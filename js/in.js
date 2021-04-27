e166d36ed2fe2694ba040dfdd3608eae7a246f70
Baikeryoyo <2425206200@qq.com>
2021-04-27 10:01:51 +0800

    Site updated: 2021-04-27 10:01:48

--- js/in.js
+++ js/in.js
@@ -1,5 +1,19 @@
 /*添加图片top*/
 var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-80px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
-document.getElementById("go-up").innerHTML += top_up;
-document.getElementById("go-up").innerHTML += rightSideFn.scrollToTop();
+// 当网页向下滑动 20px 出现"返回顶部" 按钮
+window.onscroll = function() {scrollFunction()};
+ 
+function scrollFunction() {console.log(121);
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("go-up").style.display = "block";
+    } else {
+        document.getElementById("go-up").style.display = "none";
+    }
+}
+ 
+// 点击按钮，返回顶部
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
